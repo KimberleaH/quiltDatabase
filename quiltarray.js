6ccbe82a26c17e183361f92da3a81a6b4a591364
Kimberlea Heili <62309455+KimberleaH@user.noreply.github.com>
2025-08-29 23:47:10 -0500

    Add javascript and more patterns

---
+++ quiltarray.js
@@ -0,0 +1,500 @@
+/* Available categories:
+    Owned
+    Free
+    Paid
+    No Pattern */
+
+
+
+const quiltArray = [
+   /* -------------- OWNED PATTERNS -------------- */
+   {title: "Whirlpool",
+    thumbnail: "patterns/image11.jpg",
+    url: "https://www.shabbyfabrics.com/p137388/whirlpool-quilt-pattern/",
+    category: "Owned"},
+   {title: "Sparrows",
+    thumbnail: "patterns/image1.jpg",
+    url: "https://penandpaperpatterns.com/products/printed-sparrows-quilt-pattern/",
+    category: "Owned"},
+   {title: "Squares",
+    thumbnail: "patterns/image16.jpg",
+    url: "#",
+    category: "Owned"},
+   {title: "Adventureland",
+    thumbnail: "patterns/image7.jpg",
+    url: "https://suzyquilts.com/shop/adventureland-quilt-pattern-download/",
+    category: "Owned"},
+
+
+
+
+   /* -------------- FREE PATTERNS -------------- */
+   {title: "Spectrum",
+    thumbnail: "patterns/image2.jpg",
+    url: "https://liveartgalleryfabrics.com/wp-content/uploads/2016/04/Sprectrum_Instructions.pdf",
+    category: "Free"},
+   {title: "Simple Strips",
+    thumbnail: "patterns/image4.jpg",
+    url: "https://www.polkadotchair.com/how-to-make-a-simple-strip-quilt/",
+    category: "Free"},    
+   {title: "Supernova",
+    thumbnail: "patterns/image22.jpg",
+    url: "https://confessionsofafabricaddict.blogspot.com/2017/02/stunning-stars-4-supernova.html",
+    category: "Free"},
+   {title: "Comet",
+    thumbnail: "patterns/image12.jpg",
+    url: "https://my.modafabrics.com/sites/default/files/fp_rss_comet-quilt.pdf",
+    category: "Free"},    
+   {title: "Pattern",
+    thumbnail: "patterns/image25.jpg",
+    url: "#",
+    category: "Free"},
+   {title: "Love",
+    thumbnail: "patterns/image9.jpg",
+    url: "https://designsbyktgreen.com/hst-love-quilt-finale/",
+    category: "Free"},    
+   {title: "Squash Blossom",
+    thumbnail: "patterns/image8.jpg",
+    url: "https://quietplaydesigns.com/blogs/blog/no-hst-squash-blossom-block",
+    category: "Free"},    
+   {title: "Water Waves",
+    thumbnail: "patterns/image3.jpg",
+    url: "https://mustlovequilts.com/2013/10/24/waterwaves-tutorial/comment-page-1/#comment-60",
+    category: "Free"},
+   {title: "Dragon Scale",
+    thumbnail: "patterns/image6.jpg",
+    url: "https://www.instructables.com/Dragon-Scale-Quilt/",
+    category: "Free"},
+   {title: "Houndstooth",
+    thumbnail: "",
+    url: "https://awonderfulthought.com/houndstooth-quilt-pattern/",
+    category: "Free"},
+   {title: "Scrappy Rainbow",
+    thumbnail: "",
+    url: "https://theflemingsnine.blogspot.com/2021/06/scrappy-rainbow-quilt.html",
+    category: "Free"},
+   {title: "Valley Song",
+    thumbnail: "patterns/image5.jpg",
+    url: "https://www.loandbeholdstitchery.com/blogs/lo-and-behold/valley-song-quilt-pattern-all-the-details",
+    category: "Free"},
+
+
+
+
+   /* -------------- PAID PATTERNS -------------- */
+   {title: "Carbide",
+   thumbnail: "patterns/image13.jpg",
+   url: "https://modernmoonquiltstudio.com/the-ultimate-modern-art-deco-quilt-pattern/",
+    category: "Paid"},
+   {title: "Lucky Star",
+    thumbnail: "patterns/image46.jpg",
+    url: "https://simple-simon-and-company.myshopify.com/products/pattern-pdf-the-lucky-star-quilt-pattern",
+    category: "Paid"},
+   {title: "Entropy",
+    thumbnail: "patterns/image10.jpg",
+    url: "https://www.etsy.com/listing/1433438935/entropy-bed-sizes-quilt-pattern-pdf",
+    category: "Paid"},
+   {title: "Molinos",
+    thumbnail: "patterns/image29.jpg",
+    url: "https://www.etsy.com/listing/1722643287/molinos-modern-quilt-pattern-pdf-digital",
+    category: "Paid"},
+   {title: "Night Sky",
+    thumbnail: "patterns/image33.jpg",
+    url: "https://www.etsy.com/listing/710000999/nightsky-quilt-tutorial",
+    category: "Paid"},
+   {title: "Pathways",
+    thumbnail: "patterns/image28.jpg",
+    url: "https://www.etsy.com/listing/1389942696/pathways-2023-queen-grey-blue-colourway",
+    category: "Paid"},
+   {title: "Diamond Tides",
+    thumbnail: "patterns/image31.jpg",
+    url: "https://www.etsy.com/listing/1386982678/diamond-tides-quilt-pattern",
+    category: "Paid"},  
+   {title: "River Rapids",
+    thumbnail: "patterns/image19.jpg",
+    url: "https://www.etsy.com/listing/695431351/jelly-roll-quilt-pattern-riding-the",
+    category: "Paid"},
+   {title: "Bass Boogie",
+    thumbnail: "patterns/image24.jpg",
+    url: "https://www.etsy.com/listing/1644478800/bass-boogie-jelly-roll-quilt-pattern",
+    category: "Paid"},
+   {title: "Treble Tango",
+    thumbnail: "patterns/image30.jpg",
+    url: "https://www.etsy.com/listing/1726483270/treble-tango-jelly-roll-quilt-pattern",
+    category: "Paid"},
+   {title: "Low Tide",
+    thumbnail: "patterns/image36.jpg",
+    url: "https://www.etsy.com/listing/774261972/low-tide-jelly-roll-quilt-pattern",
+    category: "Paid"},
+   {title: "Funny Crows",
+    thumbnail: "patterns/image52.jpg",
+    url: "https://www.etsy.com/listing/1727887093/funny-crows-quilt-blanket-pattern-pdf",
+    category: "Paid"},
+   {title: "Modern Baby Quilt",
+    thumbnail: "patterns/image34.jpg",
+    url: "https://www.etsy.com/listing/1230778653/modern-baby-boy-quilt-pattern-only",
+    category: "Paid"},
+   {title: "The Escapist",
+    thumbnail: "patterns/image17.jpg",
+    url: "https://www.etsy.com/listing/1217061086/the-escapist-quilt-pattern-pdf-digital",
+    category: "Paid"},
+   {title: "Pine Falls",
+    thumbnail: "patterns/image41.jpg",
+    url: "https://www.etsy.com/listing/696200758/pine-falls-quilt-pattern-pdf-download",
+    category: "Paid"},
+   {title: "Cross Track",
+    thumbnail: "patterns/image20.jpg",
+    url: "https://www.etsy.com/listing/1442788064/cross-track-quilt-pattern-pdf-download",
+    category: "Paid"},  
+   {title: "Name",
+    thumbnail: "patterns/image18.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image15.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image40.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image21.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image26.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image35.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image23.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image39.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image43.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image57.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image42.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image50.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image45.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image49.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image27.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image54.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image37.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image51.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image63.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image32.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image44.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image38.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image48.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image58.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image47.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image74.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image69.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image66.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image78.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image60.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image61.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image67.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image62.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image59.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image71.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image72.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image76.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image68.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image53.jpg",
+    url: "#",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://www.etsy.com/listing/1799142679/6-sizes-ombre-mountains-quilt-pattern?ref=collection_page",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://quiltingbookspatternsandnotions.com/products/1108-downloadable-fishscape?variant=41932043452439",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "patterns/image55.jpg",
+    url: "https://www.toadandsew.com/products/boothscornerquilt",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://www.missouriquiltco.com/products/bargello-quilt-pattern-by-missouri-star",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://www.annemariechany.com/pdf-patterns/gatekeeper-pdf-download?utm_medium=social&utm_source=pinterest&utm_campaign=tailwind_tribes&utm_content=tribes&utm_term=342464530_10559330_234169",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://www.connectingthreads.com/midas-tiles-pattern-download/p/N2664",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://quiltingbookspatternsandnotions.com/products/strip-cocoon",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://colorgirlquilts.com/products/square-off-quilt-pattern",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://olliefabrics.com/products/astra-quilt-paper-pattern",
+    category: "Paid"},
+   {title: "Name",
+    thumbnail: "",
+    url: "https://www.sewingpartsonline.com/products/cut-loose-press-tidepools-quilt-pattern?variant=46900968227103",
+    category: "Paid"},
+
+
+
+
+   /* -------------- NO PATTERNS -------------- */
+   {title: "Name",
+    thumbnail: "patterns/image79.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image77.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image75.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image65.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image64.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image70.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image56.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image80.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image73.jpg",
+    url: "#",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image82.png",
+    url: "#",
+    category: "No Pattern"},  
+   {title: "Name",
+    thumbnail: "patterns/image81.png",
+    url: "",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image83.png",
+    url: "",
+    category: "No Pattern"},
+   {title: "Name",
+    thumbnail: "patterns/image84.png",
+    url: "",
+    category: "No Pattern"},
+];
+
+
+
+
+    /* assigns an element to a variable */
+let ownedQuilts = document.getElementById("owned");
+let freeQuilts = document.getElementById("free");
+let paidQuilts = document.getElementById("paid");
+let noPatternQuilts = document.getElementById("noPattern");
+
+
+
+/* ------------ OWNED PATTERNS ------------ */
+quiltArray.forEach(quiltPattern => { // This loops through the array and is a function
+  if (quiltPattern.category === "Owned" && quiltPattern.thumbnail !== "") {
+    let html =
+    `<div class="pattern">
+       <img src="${quiltPattern.thumbnail}">
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    ownedQuilts.innerHTML += html;
+  } else if (quiltPattern.category === "Owned" && quiltPattern.thumbnail === "") {
+    let html =
+    `<div class="pattern">
+       <div class="placeholder">No Image Yet</div>
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    ownedQuilts.innerHTML += html;
+  }
+});
+
+
+/* ------------ FREE PATTERNS ------------ */
+quiltArray.forEach(quiltPattern => {
+  if (quiltPattern.category === "Free" && quiltPattern.thumbnail !== "") {
+    let html =
+    `<div class="pattern">
+       <img src="${quiltPattern.thumbnail}">
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    freeQuilts.innerHTML += html;
+  } else if (quiltPattern.category === "Free" && quiltPattern.thumbnail === "") {
+    let html =
+    `<div class="pattern">
+       <div class="placeholder">No Image Yet</div>
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    freeQuilts.innerHTML += html;
+  };
+});
+
+
+/* ------------ PAID PATTERNS ------------ */
+quiltArray.forEach(quiltPattern => {
+  if (quiltPattern.category === "Paid" && quiltPattern.thumbnail !== "") {
+    let html =
+    `<div class="pattern">
+       <img src="${quiltPattern.thumbnail}">
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    paidQuilts.innerHTML += html;
+  } else if (quiltPattern.category === "Paid" && quiltPattern.thumbnail === "") {
+    let html =
+    `<div class="pattern">
+       <div class="placeholder">No Image Yet</div>
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    paidQuilts.innerHTML += html;
+  };
+});
+
+
+/* ------------ QUILTS W/ NO PATTERN ------------ */
+quiltArray.forEach(quiltPattern => {
+  if (quiltPattern.category === "No Pattern" && quiltPattern.thumbnail !== "") {
+    let html =
+    `<div class="pattern">
+       <img src="${quiltPattern.thumbnail}">
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    noPatternQuilts.innerHTML += html;
+  } else if (quiltPattern.category === "No Pattern" && quiltPattern.thumbnail === "") {
+    let html =
+    `<div class="pattern">
+       <div class="placeholder">No Image Yet</div>
+       <a class="pattern-link link" href="${quiltPattern.url}" target="_blank">${quiltPattern.title}</a>
+    </div>`;
+
+    noPatternQuilts.innerHTML += html;
+  };
+});
--- readme.txt
+++ readme.txt
@@ -3,8 +3,9 @@ Kimberlea Heili
 
 A little database to keep track of the patterns I have purchased or am interested in.
 
-This project uses HTML, CSS, PHP, and JavaScript. (index.php, index.view.php)
+This project uses HTML, CSS, PHP, and JS. (index.php, index.view.php)
+Will include the ability to manage patterns - remove, delete, add, and edit
 
 --
 
-index.html is for display purposes only! This does not use PHP.
+index.html is for display purposes only! This does not use PHP or JS.
